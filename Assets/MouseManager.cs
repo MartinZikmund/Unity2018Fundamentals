888888ee26a69fea03227b2a8e653ad2bf3fa676
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-06 16:00:46 +0100

    Prototyping mouse manager

---
+++ Assets/MouseManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    [SerializeField] private LayerMask _clickableLayer;
+
+    [SerializeField] private Texture2D _normalPointer;
+    [SerializeField] private Texture2D _targetPointer;
+    [SerializeField] private Texture2D _doorwayPointer;
+    [SerializeField] private Texture2D _combatPointer;
+
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 50,
+            _clickableLayer.value))
+        {
+            var door = false;
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(_doorwayPointer, new Vector2(16, 16), CursorMode.Auto);
+            }
+            else
+            {
+                Cursor.SetCursor(_targetPointer, new Vector2(16, 16), CursorMode.Auto);
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(_normalPointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
